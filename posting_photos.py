dc5b4af7e9ef53c5d6d5672ea9ceff42d6bb3f27
Deanon45 <rodionovs45qwe@gmail.com>
2018-11-25 22:02:37 +0400

    First commit
    ///

---
+++ posting_photos.py
@@ -0,0 +1,30 @@
+import os
+import vk
+import settings
+import time
+from posting_photo_with_delay import post_photo_with_delay
+from uploading_photo_to_album import upload_photo_to_album
+from deleting_photos import delete_photo
+from checking_delay import delay_of_first_post
+
+first_post_delay = delay_of_first_post()
+
+all_photos = [file_name
+              for file_name in os.listdir(settings.directory_with_photos)
+              if os.path.splitext(file_name)[1] in settings.photo_formats]
+
+cur_delay = 0
+
+for photo_name in all_photos:
+    photo_path = settings.directory_with_photos + '/' + photo_name
+    photo_parameters = upload_photo_to_album(photo_path)
+
+    try:
+        post_photo_with_delay(photo_parameters, cur_delay, first_post_delay)
+        if settings.delete_photos_after_posting:
+            delete_photo(photo_path)
+    except Exception as e:
+        print(e)
+
+    cur_delay += settings.posts_interval
+    time.sleep(settings.requests_interval)
---
+++ settings.py
@@ -0,0 +1,18 @@
+group_for_photos_id = '' # ID группы для выгрузки фото в альбом
+group_for_posting_id = '' # ID группы для размещения постов
+
+album_id = '' # ID альбома
+user_access_token = '' #Токен пользователя
+
+vk_api_version = 5.75
+
+#in seconds
+posts_interval = 7200
+requests_interval = 0.5
+
+directory_with_photos = '' #[Название папки с фотографиями]
+
+#delete photo from directory after posting or not
+delete_photos_after_posting = True
+
+photo_formats = ['.jpg', '.png', '.jpeg']
