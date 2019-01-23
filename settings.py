dc55ed5afcdb15f1e4c968779993ba90aaad1cdb
Denis <40057496+DeanonQQ@users.noreply.github.com>
2019-01-24 00:56:36 +0400

    Second commit
    Add documentation
    Add install package script

--- settings.py
+++ settings.py
@@ -1,18 +1,13 @@
-group_for_photos_id = '' # ID группы для выгрузки фото в альбом
-group_for_posting_id = '' # ID группы для размещения постов
+group_for_photos_id = '111111111' # ID группы для выгрузки фото в альбом
+album_id = '111111111' # ID альбома из группы для выгрузки фото в альбом
+group_for_posting_id = '111111111' # ID группы для размещения постов
+user_access_token = '793234ff0e4d91fcca4236e4b7d1e3f30c112c9а462b05bbeab545c3c27d9935bc4bd15beb79dc454e059' #Токен пользователя
+directory_with_photos = 'cats' #[Название папки с фотографиями] Создай в папке с программой папку с таким названием и кидай туда фотки.
+posts_interval = 7200 #Через сколько времени в секундах постить следующий пост. 7200 - 2 часа
 
-album_id = '' # ID альбома
-user_access_token = '' #Токен пользователя
-
+#Это указывать необязательно
 vk_api_version = 5.75
-
-#in seconds
-posts_interval = 7200
 requests_interval = 0.5
-
-directory_with_photos = '' #[Название папки с фотографиями]
-
 #delete photo from directory after posting or not
 delete_photos_after_posting = True
-
 photo_formats = ['.jpg', '.png', '.jpeg']
